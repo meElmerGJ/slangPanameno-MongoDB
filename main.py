a6b4c1f088dd51be405b154e34652ccbb8cf15a3
Elmer Gonzalez <elmer.gonzalez@uip.edu.pa>
2023-07-30 22:42:09 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+import pymongo
+
+database_name = 'temp1'
+
+client = pymongo.MongoClient('mongodb://localhost:27017')
+
+db = client[database_name]
+
+slangs = db['slangs']
+
+if __name__ == '__main__':
+    # Elmer es
+    slangs.insert_one(
+        {
+            "name": "Elmer",
+            "age": 23
+        }
+    )
+
+
+
+
+
+
